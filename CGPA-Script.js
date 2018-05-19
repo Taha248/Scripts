ecd15eedc7e57e0f54d732fe40417ccc205e15e9
Taha <taha24888@gmail.com>
2018-05-19 05:22:46 +0500

    IULMS CGPA Script v1.0

--- CGPA-Script.js
+++ CGPA-Script.js
@@ -2,13 +2,10 @@
     $(document).ready(function(){
         var button = document.createElement("button");
 button.innerHTML = "Stats";
-//button.setAttribute("style","position:relative");
-// 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
         var p = document.getElementById("degreeSelection");
 p.appendChild(button);
 
-// 3. Add event handler
 button.addEventListener ("click", function() {
    var rowLenght =   $(".transcript-content").length; 
         var y =  $("tbody .transcript-content").length;
@@ -20,7 +17,6 @@ button.addEventListener ("click", function() {
         var CrHr =  $("tbody .transcript-content").children().eq(val-2).html(); 
         var Grade =  $("tbody .transcript-content").children().eq(val-1).html(); 
             
-          //	var FailedCount=0,A=0,Bplus=0,B=0,Cplus=0,C=0;
     switch(Grade) {
         case "A":
             A++;
@@ -58,12 +54,9 @@ button.addEventListener ("click", function() {
             
          val+=5;
             }
-        //alert("Total GPA: "+totalWeightedGPA+ "Total Credit Hour" +totalCreditHours);
         var CGPA = totalWeightedGPA/totalCreditHours;
         alert("CGPA : "+Math.round(CGPA*100)/100 +" \nNumber of A= "+A+ "\nNumber of B+= "+Bplus+"\nNumber of B= "+B+"\nNumber of C+ ="+Cplus+"\nNumber of C= "+C+"\nFailed Courses ="+FailedCount+"\nTotal Credit Hour = "+totalCreditHours+ "\nTotal Courses= "+totalCourses );
             
-  //      }
 });
-//        for(var i = 0 ; i<=999;i++){
          
     });
